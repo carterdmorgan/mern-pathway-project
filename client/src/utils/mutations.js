0b3a2ac0e9b79c4fcce25cef10caccf18a995623
Carter Morgan <carterdmorgan@gmail.com>
2022-02-12 21:46:26 -0500

    finished backend, working on frontend now

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookInput: BookInput!) {
+    saveBook(bookInput: $bookInput) {
+      _id,
+      username
+      bookCount
+      savedBooks {
+        bookId
+      }
+    }
+  }
+`
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        username
+      }
+    }
+  }
+`
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        username
+      }
+    }
+  }
+`
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookInput: BookInput!) {
+    saveBook(bookInput: $bookInput) {
+      _id,
+      username
+      bookCount
+      savedBooks {
+        bookId
+      }
+    }
+  }
+`
